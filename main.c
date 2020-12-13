dd497500c117a06d0d666db4bc4427fd2ecfd107
ArinaFilatova25 <75203387+ArinaFilatova25@users.noreply.github.com>
2020-12-13 17:51:15 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,109 +1,359 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
 
-void transform(int k, int n, float **matr, int *res)
+void res(float** arr, int n, float** a)
 {
-    int i, j;
-    int imax = k, jmax = k;
 
-    for (i = k; i < n; i++)
-        for (j = k; j < n; j++)
-            if (fabs(matr[imax][jmax]) < fabs(matr[i][j]))
-            {
-                imax = i;
-
-                jmax = j;
-
-            }
-
-    for (j = k; j < n + 1; j++)
+    int k = 0;
+    float m;
+    for (k = n - 1; k > 0; k--)
     {
-        float t = matr[k][j];
-        matr[k][j] = matr[imax][j];
-        matr[imax][j] = t;
+        for (int j = k - 1; j >= 0; j--)
+        {
+            m = arr[j][k];
+            arr[j][k] -= arr[k][k] * m;
+            for (int i = n - 1; i >= 0; i--)
+                a[j][i] -= a[k][i] * m;
+        }
     }
 
-    for (i = 0; i < n; i++)
-    {
-        float t = matr[i][k];
-        matr[i][k] = matr[i][jmax];
-        matr[i][jmax] = t;
-    }
-    i = res[k];
-
-    res[k] = res[jmax];
-
-    res[jmax] = i;
 }
 
-int main()
+
+void Copy(float** arr1, float** arr2, int n)
 {
-    setlocale(LC_ALL, "Rus");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr2[i][j] = arr1[i][j];
+        }
+    }
 
-    float **matr;
-    float *x;
-    int i,j,k;
-    int *res;
-    int n;
+}
 
-    printf("Введите кол-во уравнений\n");
-    scanf("%d", &n);
+float** Proizv(float** arr1, float** arr2, int n)
+{
 
-    matr = (float**) malloc(n*sizeof(float*));
-    x = (float*) malloc(n*sizeof(float));
-    res = (int*) malloc(n*sizeof(int));
+    float** p = (float**)malloc(n * sizeof(float*));
 
     for (int i = 0; i < n; i++)
     {
-        matr[i] = (float*) malloc((n+1)*sizeof(float));
+        p[i] = (float*)malloc(n * sizeof(float));
     }
-
-    printf("Введите строки матрицы\n");
-    for (i = 0; i < n; i++)
-        for (j = 0; j <= n; j++)
-            scanf("%f", &matr[i][j]);
-
-    for (i = 0; i < n; i++)
-        res[i] = i;
-
-    for (k = 0; k < n; k++)
+    for (int i = 0; i < n; i++)
     {
-        transform(k, n, matr, res);
-
-        if (fabs(matr[k][k]) < 0.00001 )
+        for (int j = 0; j < n; j++)
         {
-            printf("");
-            return 0;
+            p[i][j] = 0;
+
+            for (int k = 0; k < n; k++)
+                p[i][j] += arr1[i][k] * arr2[k][j];
+
         }
 
-        for (j = n; j >= k; j--)
-            matr[k][j]= matr[k][j]/matr[k][k];
+    }
+    return p;
+}
+float detcalc(float** arr, int n, int sign)
+{
+
+    float det = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                det *= arr[i][j];
+            }
+        }
+    }
+
+    return det * sign;
+
+}
+float** trig_matr2(float** arr, int n, int* sign)
+{
+
+    int k = 0, ind;
+    float t_ind;
+    while (k < n)
+    {
+        if (arr[k][k] == 0)
+        {
+            *sign = -1;
+            ind= 0;
+            for (int i = k + 1; i < n; i++)
+            {
+                if (arr[i][k] != 0)
+                {
+                    ind = i;
+                    break;
+                }
+            }
+
+            if (ind== 0)
+            {
+
+                return NULL;
+            }
+
+            else
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    float t;
+                    t=arr[k][j];
+                    arr[k][j]=arr[ind][j];
+                    arr[ind][j]=t;
+                }
+            }
+        }
+
+        t_ind= arr[k][k];
+        for (int i = k + 1; i < n; i++)
+        {
+            float m= arr[i][k];
+            for (int j = 0; j < n; j++)
+                arr[i][j] = arr[i][j] - (arr[k][j] / t_ind) * m;
+        }
+        k++;
+        printf("\n");
+    }
+
+    return arr;
+}
 
 
-        for (i = k+ 1; i < n; i++)
-            for (j = n; j >= k; j--)
-                matr[i][j] = matr[i][j] - (matr[k][j] * matr[i][k]);
+float** trig_matr(float** arr, int n, float** a)
+{
+
+    int k = 0, ind;
+    float t_ind;
+    while (k < n)
+    {
+        if (arr[k][k] == 0)
+        {
+            ind = 0;
+            for (int i = k + 1; i < n; i++)
+            {
+                if (arr[i][k] != 0)
+                {
+                    ind= i;
+                    break;
+                }
+            }
+
+            if (ind== 0)
+            {
+
+                return NULL;
+            }
+
+            else
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    float t,temp;
+                    t=arr[k][j];
+                    arr[k][j]=arr[ind][j];
+                    arr[ind][j]=t;
+
+                    temp=a[k][j];
+                    a[k][j]=a[ind][j];
+                    a[ind][j]=temp;
+                }
+            }
+
+        }
+
+
+        t_ind= arr[k][k];
+
+        for (int j = 0; j < n; j++)
+        {
+            arr[k][j] = arr[k][j] / t_ind;
+            a[k][j] = a[k][j] / t_ind;
+        }
+
+        for (int i = k + 1; i < n; i++)
+        {
+
+            int m= arr[i][k];
+            for (int j = 0; j < n; j++)
+            {
+                arr[i][j] = arr[i][j] - arr[k][j] * m;
+                a[i][j] = a[i][j] - a[k][j] * m;
+            }
+        }
+        k++;
+    }
+
+    return a;
+}
+
+
+void task9_7()
+{
+    int n;
+    printf("Введите кол-во строк и столбцов:\n ");
+    scanf("%d", &n);
+
+    float det = 0;
+
+    float** arr = (float**)malloc(n * sizeof(float*));
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = (float*)malloc(n * sizeof(float));
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("arr[%i][%i] = ", i, j);
+            scanf("%f", &arr[i][j]);
+        }
 
     }
 
-    for ( i = 0; i < n; i++)
-        x[i] = matr[i][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%f ", arr[i][j]);
+        }
+        printf("\n");
+    }
 
-    for (i = n - 2; i >= 0; i--)
-        for (j = i + 1; j < n; j++)
-            x[i]= x[i] - (x[j] * matr[i][j]);
+    int sign = 1;
+    arr = trig_matr2(arr, n,&sign);
+    if (arr == NULL)
+    {
+        printf("Определитель матрицы 0\n ");
+    }
+    else
+    {
+        det = detcalc(arr, n, sign);
 
-    printf("\n");
+        printf("Определитель матрицы :\n ");
+        printf("%f", det);
+    }
 
-    printf("Решение системы:\n");
-    for (i = 0; i < n; i++)
-        for (j = 0; j <= n; j++)
 
-            if (i == res[j])
+}
+
+void task9_6()
+{
+    int n;
+    printf("Введите кол-во строк и столбцов:\n ");
+    scanf("%d", &n);
+
+    float** arr = (float**)malloc(n * sizeof(float*));
+    float** arr_copy = (float**)malloc(n * sizeof(float*));
+    float** Edmatr = (float**)malloc(n * sizeof(float*));
+    float** ans= (float**)malloc(n * sizeof(float*));
+
+    for (int i = 0; i < n; i++)
+    {
+        arr [i]= (float**)malloc(n * sizeof(float*));
+        arr_copy [i]= (float**)malloc(n * sizeof(float*));
+        Edmatr[i]= (float**)malloc(n * sizeof(float*));
+        ans[i]= (float**)malloc(n * sizeof(float*));
+
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("arr[%d][%d] = ", i, j);
+            scanf("%f", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            if (i == j)
             {
-                printf("%f\n", x[j]);
-                break;
+                Edmatr[i][j] = 1;
             }
-    return 0;
+            else Edmatr[i][j] = 0;
+        }
+    }
+    Copy(arr, arr_copy, n);
+    if (trig_matr(arr, n, Edmatr) != NULL)
+    {
+
+
+        res(arr, n, Edmatr);
+        printf("\nОбратная матрица\n");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                printf("%f ", Edmatr[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nПроверка\n");
+        printf("A * A^-1 = ");
+        printf("\n");
+        ans = Proizv(Edmatr, arr_copy, n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                printf("%f ", ans[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("Решений нет");
+    }
+
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n =3 ;
+
+    while (n != 0)
+    {
+        printf("\nВведите номер задания\n");
+        printf("\n1-9_6, 2-9_7, 0-выход\n");
+        scanf("%d", &n);
+
+        switch (n)
+        {
+        case 1:
+            task9_6();
+
+            break;
+
+        case 2:
+            task9_7();
+            break;
+            break;
+        default:
+            n = 0;
+            break;
+        }
+
+    }
+
 }
